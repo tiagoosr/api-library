ddafd9289370335128c5158d2edc62b0b76b66eb
Tiago Rodrigues <tiiago.santosr@gmail.com>
2024-07-29 18:52:55 -0300

    chore: project config

---
+++ src/main/java/com/project/api_library/api_library/ApiLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.project.api_library.api_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/api_library/api_library/ApiLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.api_library.api_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
